ed115df3ed33744cc5e8a3773a91d92b9f1cdd43
Arman Bilge <armanbilge@gmail.com>
2015-09-22 17:07:16 +1200

    Finish assignment 4

--- triangles.py
+++ triangles.py
@@ -1,8 +1,12 @@
 import sys
 import itertools as it
 import operator as op
+from collections import namedtuple
 
-def repeatfunc(func, times):
-    return it.starmap(func, it.repeat((), times))
-
-T = sorted(tuple(sorted(l[:3])), l[-1] for l in map(str.split, repeatfunc(sys.stdin.readline, int(sys.stdin.readline()))), key=op.itemgetter(1))
+Triangle = namedtuple('Triangle', ('a', 'b', 'c', 'g'))
+triangle = sorted(it.chain.from_iterable((Triangle(x, y, z, g) for x, y, z in it.permutations((a, b, c))) for a, b, c, g in map(str.split, it.starmap(sys.stdin.readline, it.repeat((), int(sys.stdin.readline()))))), key=op.attrgetter('g'))
+table = [0] * len(triangle)
+table[0] = 1
+for i in range(1, len(table)):
+    table[i] = 1 + max(table[j] if triangle[i].a == triangle[j].c and triangle[i].g > triangle[j].g else 0 for j in range(i))
+print(max(table))
