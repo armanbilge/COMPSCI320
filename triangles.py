18a4286053c090ecfd696d443550a9d2958ff49c
Arman Bilge <armanbilge@gmail.com>
2015-09-22 23:01:05 +1200

    Optimize triangles implementation

--- triangles.py
+++ triangles.py
@@ -3,10 +3,10 @@ import itertools as it
 import operator as op
 from collections import namedtuple
 
-Triangle = namedtuple('Triangle', ('a', 'b', 'c', 'g'))
-triangle = sorted(it.chain.from_iterable((Triangle(x, y, z, g) for x, y, z in it.permutations((a, b, c))) for a, b, c, g in map(str.split, it.starmap(sys.stdin.readline, it.repeat((), int(sys.stdin.readline()))))), key=op.attrgetter('g'))
-table = [0] * len(triangle)
-table[0] = 1
-for i in range(1, len(table)):
-    table[i] = 1 + max(table[j] if triangle[i].a == triangle[j].c and triangle[i].g > triangle[j].g else 0 for j in range(i))
+Triangle = namedtuple('Triangle', ('a', 'b', 'g'))
+triangle = sorted(set(it.chain.from_iterable((Triangle(x, y, g) for x, y, _ in it.permutations((a, b, c))) for a, b, c, g in map(str.split, it.starmap(sys.stdin.readline, it.repeat((), int(sys.stdin.readline())))))), key=op.attrgetter('g'))
+table = [1] * len(triangle)
+for i, ti in enumerate(triangle[1:], 1):
+    tia, tig = ti.a, ti.g
+    table[i] = 1 + max(x if tia == tj.b and tig > tj.g else 0 for x, tj in zip(table, triangle[:i]))
 print(max(table))
